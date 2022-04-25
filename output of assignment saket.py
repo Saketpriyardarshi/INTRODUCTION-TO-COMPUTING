862bcb03bbf26dda048493eaebc6907cd164487c
Saketpriyardarshi <104380198+Saketpriyardarshi@users.noreply.github.com>
2022-04-25 22:13:49 +0530

    Add files via upload

---
+++ output of assignment saket.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:/python work/assignment saket.py =================
+
+Answer for ques1:
+Enter number 1 :12
+Enter number 2:24
+Enter number 3:23
+
+The average is: 19.666666666666668
+
+Answer for ques2:
+Enter your income($):100000
+Enter number of dependents in your family:3
+
+tax on your income is: 16200.0
+
+Answer for ques3:
+/nEnter number of seconds : 120
+It has 2 minutes and  0 seconds
+
+Answer for ques4:
+75.0 <class 'str'>
+
+Answer for ques5:
+cosine( 0 ) is 1.0            sine( 0 ) is 0.0
+cosine( 15 ) is 0.9659            sine( 15 ) is 0.2588
+cosine( 30 ) is 0.866            sine( 30 ) is 0.5
+cosine( 45 ) is 0.7071            sine( 45 ) is 0.7071
+cosine( 60 ) is 0.5            sine( 60 ) is 0.866
+cosine( 75 ) is 0.2588            sine( 75 ) is 0.9659
+cosine( 90 ) is 0.0            sine( 90 ) is 1.0
+cosine( 105 ) is -0.2588            sine( 105 ) is 0.9659
+cosine( 120 ) is -0.5            sine( 120 ) is 0.866
+cosine( 135 ) is -0.7071            sine( 135 ) is 0.7071
+cosine( 150 ) is -0.866            sine( 150 ) is 0.5
+cosine( 165 ) is -0.9659            sine( 165 ) is 0.2588
+cosine( 180 ) is -1.0            sine( 180 ) is 0.0
+cosine( 195 ) is -0.9659            sine( 195 ) is -0.2588
+cosine( 210 ) is -0.866            sine( 210 ) is -0.5
+cosine( 225 ) is -0.7071            sine( 225 ) is -0.7071
+cosine( 240 ) is -0.5            sine( 240 ) is -0.866
+cosine( 255 ) is -0.2588            sine( 255 ) is -0.9659
+cosine( 270 ) is -0.0            sine( 270 ) is -1.0
+cosine( 285 ) is 0.2588            sine( 285 ) is -0.9659
+cosine( 300 ) is 0.5            sine( 300 ) is -0.866
+cosine( 315 ) is 0.7071            sine( 315 ) is -0.7071
+cosine( 330 ) is 0.866            sine( 330 ) is -0.5
+cosine( 345 ) is 0.9659            sine( 345 ) is -0.2588
